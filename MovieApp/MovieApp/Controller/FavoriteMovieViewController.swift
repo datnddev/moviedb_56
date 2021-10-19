b6d1ce97e726e0d7446df4f2eca5d3a936152c7d
Dat Nguyen <datnd.dev@gmail.com>
2021-10-19 17:07:57 +0700

    create tab bar and handle tab bar

---
+++ MovieApp/MovieApp/Controller/FavoriteMovieViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteMovieViewController.swift
+//  MovieApp
+//
+//  Created by datNguyem on 19/10/2021.
+//
+
+import UIKit
+
+final class FavoriteMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
