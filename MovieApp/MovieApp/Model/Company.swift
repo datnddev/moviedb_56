c78a02bd1f2a02f84681a5397c4a70c4e3489caa
Dat Nguyen <datnd.dev@gmail.com>
2021-10-21 08:34:01 +0700

    create api class

--- MovieApp/MovieApp/Model/Company.swift
+++ MovieApp/MovieApp/Model/Company.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Company: Decodable {
     var id: Int
     var name: String
-    var logoPath: String
+    var logoPath: String?
     
     enum Codingkeys: String, CodingKey {
         case id
