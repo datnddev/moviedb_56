f841d9d039a5818311b3dbc83962cfbc70b1311d
Dat Nguyen <datnd.dev@gmail.com>
2021-10-28 13:42:54 +0700

    create ui detail screen

--- MovieApp/MovieApp/Extensions/UIImageView+.swift
+++ MovieApp/MovieApp/Extensions/UIImageView+.swift
@@ -9,16 +9,5 @@ import Foundation
 import UIKit
 
 extension UIImageView {
-    func loadImageUrl(path: String, completion: @escaping (UIImage?) -> ()) {
-        guard let imageURL = URL(string: Constant.getImageLink(path: path)) else { return }
-        
-        URLSession.shared.dataTask(with: imageURL) { data, _, error in
-            guard let data = data else { return }
-            let image = UIImage(data: data)
-            
-            DispatchQueue.main.async {
-                completion(image)
-            }
-        }.resume()
-    }
+
 }
---
+++ MovieApp/MovieApp/View/CollectionCell/ProducerCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  ProducerCollectionViewCell.swift
+//  MovieApp
+//
+//  Created by datNguyem on 28/10/2021.
+//
+
+import UIKit
+
+final class ProducerCollectionViewCell: UICollectionViewCell, ReusableViewProtocol {
+    @IBOutlet private weak var logoImageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    
+    private var imagesCache = Cache<NSString, UIImage>()
+    
+    func configure(company: Company) {
+        guard let logoPath = company.logoPath else {
+            nameLabel.isHidden = false
+            nameLabel.text = company.name
+            return
+        }
+        
+        let key = NSString(string: logoPath)
+        
+        if let imageCache = imagesCache.value(for: key) {
+            logoImageView.isHidden = false
+            logoImageView.image = imageCache
+        } else {
+            logoImageView.loadImageUrl(path: logoPath) { [weak self] image in
+                guard let self = self, let image = image else { return }                
+                self.imagesCache.insert(image, for: key)
+            }
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        logoImageView.image = nil
+        nameLabel.text = nil
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        logoImageView.contentMode = .scaleAspectFit
+        nameLabel.adjustsFontSizeToFitWidth = true
+    }
+}
