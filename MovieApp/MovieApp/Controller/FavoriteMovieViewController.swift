3abd5ea477f2b3c0051dbdcc03e8b62aa7266708
Dat Nguyen <datnd.dev@gmail.com>
2021-11-01 15:18:32 +0700

    favorite movie: fetching data

--- MovieApp/MovieApp/Controller/FavoriteMovieViewController.swift
+++ MovieApp/MovieApp/Controller/FavoriteMovieViewController.swift
@@ -8,8 +8,26 @@
 import UIKit
 
 final class FavoriteMovieViewController: UIViewController {
+    private var movies = [FavoriteMovie]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(loadData),
+                                               name: NSNotification.Name("dataChanged"),
+                                               object: nil)
+        loadData()
+    }
+    
+    @objc
+    private func loadData() {
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            self.movies = CoreDataManager.shared.getMovies()
+            
+            DispatchQueue.main.async {
+                //update data here
+            }
+        }
     }
 }
